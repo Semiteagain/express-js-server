b507cb8b1033c49b67d93b4787e9e6a01e8cd0b7
Mukaila Semiu <semiteagain@gmail.com>
2020-12-19 07:44:10 +0100

    setting route in expressJs

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", function (request, response) {
-    response.send("<h1>Hello World</h1>")
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World</h1>")
 })
+
+
+app.get("/contact", function (req, res) {
+    res.send("<p>Contact Me: semiteagain@gmail.com</p>")
+})
+
+app.get("/about", function (req, res) {
+    res.send("<div><h1>I am Mukaila Semiu,</h1> <p>Learning with Angela Yu.</p></div>")
+})
+
 
 app.listen(3000, function () {
     console.log("server 3000 started....")
